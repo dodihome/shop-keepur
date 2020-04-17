d4697bcda0883dc8c2ad1496dfdd0aa08c662d9d
ahmay98 <may@keepur.io>
2020-04-17 12:17:58 -1000

    moving control logic upwards, away from the low level component

--- src/components/admin/PendingClaims.tsx
+++ src/components/admin/PendingClaims.tsx
@@ -12,21 +12,12 @@ class ClaimsRow extends React.Component<any, any> {
         e.preventDefault();
 
         const claimId = e.target.value;
-        const userId = this.props.user.id;
-        const { error } = await Admin.acceptClaim (claimId, userId);
-        if (error) {
-            this.setState({error});
-        }
+        this.props.onAccept(claimId);
     }
 
     async onReject (claimId: string, reason: string, e: any) {
         e.preventDefault();
-
-        const userId = this.props.user.id;
-        const { error } = await Admin.rejectClaim (claimId, userId, reason);
-        if (error) {
-            this.setState({error});
-        }
+        this.props.onReject(claimId, reason);
     }
 
     render () {
@@ -82,15 +73,31 @@ class ClaimsRow extends React.Component<any, any> {
 export class PendingClaims extends React.Component<any, any> {
     state : any = {bizz: []} as any;
 
+    async onAccept (claimId: string) {
+        const userId = this.props.user.id;
+        const { error } = await Admin.acceptClaim (claimId, userId);
+        this._load();
+    }
+
+    async onReject (claimId: string, reason: string) {
+        const userId = this.props.user.id;
+        const { error } = await Admin.rejectClaim (claimId, userId, reason);
+        this._load();
+    }
+
+    async _load () {
+        const { user } = this.props;
+        if (!user) return;
+
+        const { error, bizz } = await Admin.listPendingClaims(user.id);
+        this.setState ({
+            error, bizz
+        })
+    }
+
     async componentDidUpdate (oldProps: any, oldState: any) {
         if (this.props.user !== oldProps.user) {
-            const { user } = this.props;
-            if (!user) return;
-    
-            const { error, bizz } = await Admin.listPendingClaims(user.id);
-            this.setState ({
-                error, bizz
-            })
+            this._load();
         }
     }
 
@@ -112,7 +119,10 @@ export class PendingClaims extends React.Component<any, any> {
                     <tbody>
                         {
                             this.state.bizz.map ((biz, idx) => {
-                                return <ClaimsRow key={biz.id} user={this.props.user} biz={biz} idx={idx} history={this.props.history} />
+                                return <ClaimsRow key={biz.id} user={this.props.user} biz={biz} idx={idx} history={this.props.history} 
+                                    onAccept={this.onAccept.bind(this)}
+                                    onReject={this.onReject.bind(this)}
+                                />
                             })
                         }
                     </tbody>
